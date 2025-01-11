30fd902d9577b3bbf1668269ceb54c5e6ffa8d8b
Nayera Ibrahim <158851933+Nayeraaibrahim@users.noreply.github.com>
2025-01-11 02:50:06 +0200

    [1400] [Time Beats: 41.18%] [Memory Beats: 23.53%] - LeetPush

---
+++ 1400-Construct-K-Palindrome-Strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canConstruct(self, s, k):
+        if len(s) < k:
+            return False
+        c = Counter(s)
+        counter =0 
+        for i in c.values():
+            if i%2 !=0:
+                counter+=1
+        if counter <= k:
+            return True
+        else:
+            return False
+                
+        
