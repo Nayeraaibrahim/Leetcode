f169b834b5e306cd320279b774c26a213af33cf1
Nayera Ibrahim <158851933+Nayeraaibrahim@users.noreply.github.com>
2025-01-28 02:09:46 +0200

    [1462] [Time Beats: 89.09%] [Memory Beats: 49.09%] - LeetPush

---
+++ 1462-Course-Schedule-IV.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+        adj = [[] for _ in range(numCourses)]
+        prereq = [0] * numCourses  
+        in_degree = [0] * numCourses
+        
+        for a, b in prerequisites:
+            adj[a].append(b)
+            prereq[b] |= 1 << a  
+            in_degree[b] += 1
+        
+        q = deque()
+        for i in range(numCourses):
+            if in_degree[i] == 0:
+                q.append(i)
+        
+        while q:
+            u = q.popleft()
+            for v in adj[u]:
+                prereq[v] |= prereq[u]  # Merge u's prerequisites into v's
+                in_degree[v] -= 1
+                if in_degree[v] == 0:
+                    q.append(v)
+        
+        # Answer queries using bitmask checks
+        ans = []
+        for u, v in queries:
+            ans.append((prereq[v] & (1 << u)) != 0)
+        return ans
+
+        
