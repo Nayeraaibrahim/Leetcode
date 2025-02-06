5a1a6df0c0d72e66890b389fd8655f869c791935
Nayera Ibrahim <158851933+Nayeraaibrahim@users.noreply.github.com>
2025-02-06 02:56:53 +0200

    [1726] [Time Beats: 22.22%] [Memory Beats: 55.56%] - LeetPush

---
+++ 1726-Tuple-with-Same-Product.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def tupleSameProduct(self, nums):
+        c = 0
+        a = collections.Counter()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                m = nums[i]*nums[j]
+                c += 8*a[m]
+                a[m] +=1
+        return c
+        
